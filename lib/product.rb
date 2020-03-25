9ff8f794b2e6f0ded285d9fc88ccd5e0c6969cdc
Jane Greene <janegreenecasa@gmail.com>
2020-03-25 09:38:17 -0600

    All interaction and tests iteration 1 complete

--- lib/product.rb
+++ lib/product.rb
@@ -2,16 +2,26 @@ class Product
   attr_reader :category,
               :name,
               :unit_price,
-              :quantity
+              :quantity,
+              :hoard #not sure about this
 
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoard = false
   end
 
   def total_price
     @unit_price * @quantity.to_f
+  end
+
+  def is_hoarded?
+    @hoard
+  end
+
+  def hoard
+    @hoard = true
   end
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -35,4 +35,13 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_is_hoarded?
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+    assert_equal false, product.is_hoarded?
+
+    product.hoard
+    assert_equal true, product.is_hoarded?
+  end
 end
